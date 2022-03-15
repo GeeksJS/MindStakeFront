59be3c571ac6725aa00177ab30a25c4f3b021a4d
AchrefBarketi <achref.barketi@esprit.tn>
2022-03-15 17:48:36 +0100

    update register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -6,16 +6,58 @@ import '../Login/login.css'
 
 export default function Register() {
 
-    const [userRole, setUserRole] = useState("Simple user")
+    const [next, setNext] = useState(false)
+    const [register, setRegister] = useState(false)
+
+    const [back, setBack] = useState(false)
+
+
+    const [userRole, setUserRole] = useState("SimpleUser")
+
     const changeRole = (e) => {
 
+        console.log(userRole)
+        { document.getElementById('select').value !== "SimpleUser" && setNext(true) }
+        { document.getElementById('select').value === "SimpleUser" && setNext(false) }
+
     }
+
+
+    const handleNextClick = (e) => {
+        e.preventDefault();
+        setUserRole(document.getElementById('select').value)
+        console.log(userRole)
+        setNext(false)
+        setBack(true)
+        // setUserRole("SimpleUser")
+    }
+
+    const handleBackClick = (e) => {
+        e.preventDefault();
+        setBack(false)
+        setUserRole("SimpleUser")
+        //setNext(false)
+        document.getElementById('select').value="SimpleUser"
+        document.getElementById('cv').value=""
+        document.getElementById('selectCreator').value=""
+        document.getElementById('startup').value=""
+        document.getElementById('company').value=""
+        document.getElementById('address').value=""
+
+
+    }
+
+
+
     return (
         <div id="loginform" >
+            <a onClick={handleBackClick} style={{color:"#484848",position:'absolute',marginLeft:'-180px',marginTop:'30px'}} hidden={!back}>
+                <i className="fas fa-arrow-left fa-2x" />
+            </a>
             <img src='assets/img/logo.png' alt="logo" className='logo' />
             <div>
                 <form>
-                    <div className='rowLogin'>
+                    <div className='rowLogin' hidden={userRole !== "SimpleUser"}>
                         <label>UserName</label>
                         <input placeholder="Enter you userName" type="text" />
                         <br />
@@ -32,19 +74,47 @@ export default function Register() {
                         <input placeholder="Enter you profile picture" type="file" />
                         <br />
                         <label style={{ marginRight: '420px' }}>Role</label>
-                        <select className='select' onChange={changeRole} >
-                            <option >Select your role</option>
-                            <option  value={"Creator"}>Creator</option>
+                        <select className='select' id='select' onChange={changeRole} >
+                            <option value={"Creator"}>Creator</option>
                             <option value={"Investor"}>Investor</option>
-                            <option value={"SimpleUser"}>Simple user</option>
+                            <option selected value={"SimpleUser"}>Simple user</option>
                         </select>
                     </div>
+                    <div id='creator' className='rowLogin' hidden={userRole !== "Creator"}>
+                        <label>CV</label>
+                        <input id='cv' placeholder="Enter you Curriculum Vitae" type="file" />
+                        <br />
+                        <label >Creator Type</label>
+                        <select id='selectCreator' className='select'  >
+                            <option value={"Individual"}>Individual</option>
+                            <option value={"Startup"}>Startup</option>
+                        </select>
+                        <br />
+                        <label>Startup Name</label>
+                        <input id='startup' placeholder="Enter you Startup name" type="text" />
+                        <br />
+                    </div>
+                    <div className='rowLogin' hidden={userRole !== "Investor"}>
+                        <label>Company Name</label>
+                        <input id='company' placeholder="Enter you Company Name" type="text" />
+                        <br />
+                        <label>Address</label>
+                        <input id='address' placeholder="Enter you Address" type="text" />
+                        <br />
 
-                    <a href='/' style={{ width: '100%' }}>
+                    </div>
+
+
+                    <a href='/' style={{ width: '100%' }} hidden={next === true}>
                         <div id='button' className='rowLogin' >
                             <button >Register</button>
                         </div>
+                    </a>
 
+                    <a style={{ width: '100%' }} hidden={next === false}>
+                        <div id='button' className='rowLogin' >
+                            <button onClick={handleNextClick}>Next</button>
+                        </div>
                     </a>
                     <div className='register'>
                         <p>already have an account? &nbsp;  </p>
@@ -53,7 +123,9 @@ export default function Register() {
                 </form>
 
             </div>
-            <OtherMethods />
+            <br />
+            <br />
+            {/* <OtherMethods /> */}
         </div>
     )
 

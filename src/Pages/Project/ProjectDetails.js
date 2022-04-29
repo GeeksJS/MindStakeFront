92de5d66d78d26dc3e3bbaf49d4b4fbae25057ee
wajdi.benhajyahya@esprit.tn <wajdi.benhajyahya@esprit.tn>
2022-04-29 01:35:14 +0100

    Proposal Steps and Form

--- src/Pages/Project/ProjectDetails.js
+++ src/Pages/Project/ProjectDetails.js
@@ -351,9 +351,10 @@ export default function ProjectDetails() {
                                                 <a type="submit" className="main-btn" onClick={donation}>
                                                     Donate Now <i className="fas fa-arrow-right" />
                                                 </a>
-                                                <button type="submit" className="main-btn" style={{ backgroundColor: 'rgba(255, 180, 40)', marginLeft: '30px', marginTop: '0px' }}onClick={(e) => {e.preventDefault(); setOpenPopup(true)}}>
+                                                
+                                                <Link to={`/proposal/${id}`} type="submit" className="main-btn" style={{ backgroundColor: 'rgba(255, 180, 40)', marginLeft: '30px', marginTop: '0px' }}>
                                                     Propose to cantact <i class="fab fa-facebook-messenger"></i>
-                                                </button>
+                                                </Link>
                                             </div>
                                         }
                                         {Connected.Role === 'Creator' && Connected.userId === user._id &&
